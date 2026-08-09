ef2e328c9856166b25ad1cf79ac3ff0de32e4a32
pokemongachashop <gachashop12@gmail.com>
2026-08-09 20:21:09 +0900

    validation.ts 만들기

---
+++ src/constants/validation.ts
@@ -0,0 +1,7 @@
+export const LOGIN_ID_MIN_LENGTH = 4;
+export const LOGIN_ID_MAX_LENGTH = 20;
+
+export const PASSWORD_MIN_LENGTH = 8;
+
+export const PROBABILITY_TOTAL = 100;
+export const PROBABILITY_EPSILON = 0.000001;
