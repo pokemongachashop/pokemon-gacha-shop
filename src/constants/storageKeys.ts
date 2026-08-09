a52ff055636e0f37c886e592950b8816edcad7a7
pokemongachashop <gachashop12@gmail.com>
2026-08-09 19:00:07 +0900

    storageKeys.ts 만들기

---
+++ src/constants/storageKeys.ts
@@ -0,0 +1,9 @@
+export const STORAGE_KEYS = {
+  CURRENT_PACK_ID: 'pokemon-gacha.current-pack-id',
+  THEME: 'pokemon-gacha.theme',
+  HIDDEN_POPUP_NOTICES: 'pokemon-gacha.hidden-popup-notices',
+} as const;
+
+export const SESSION_KEYS = {
+  PENDING_DRAW_REQUEST: 'pokemon-gacha.pending-draw-request',
+} as const;
