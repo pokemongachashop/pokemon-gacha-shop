ddda8fb94081bce648471f500b0cd267083e8b1d
pokemongachashop <gachashop12@gmail.com>
2026-08-09 20:09:31 +0900

    common.ts 업데이트

--- src/types/common.ts
+++ src/types/common.ts
@@ -1,3 +1,10 @@
 export type Nullable<T> = T | null;
 
+export type Optional<T> = T | undefined;
+
 export type AsyncStatus = 'IDLE' | 'LOADING' | 'SUCCESS' | 'ERROR';
+
+export type LoadingState = {
+  status: AsyncStatus;
+  error: Nullable<string>;
+};
