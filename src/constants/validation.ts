17391debbbfddb308993cc0699fe23892e654013
pokemongachashop <gachashop12@gmail.com>
2026-08-11 03:10:27 +0900

    validation.ts 업데이트

--- src/constants/validation.ts
+++ src/constants/validation.ts
@@ -3,5 +3,8 @@ export const LOGIN_ID_MAX_LENGTH = 20;
 
 export const PASSWORD_MIN_LENGTH = 8;
 
+export const DISPLAY_NAME_MIN_LENGTH = 1;
+export const DISPLAY_NAME_MAX_LENGTH = 32;
+
 export const PROBABILITY_TOTAL = 100;
 export const PROBABILITY_EPSILON = 0.000001;
