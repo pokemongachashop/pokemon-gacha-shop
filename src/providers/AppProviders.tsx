9141b44abc58ef3fe47460e4722685200f4f8881
pokemongachashop <gachashop12@gmail.com>
2026-08-13 12:04:46 +0900

    AppProviders.tsx 만들기

---
+++ src/providers/AppProviders.tsx
@@ -0,0 +1,7 @@
+import type { PropsWithChildren } from 'react';
+
+import { AuthProvider } from './AuthProvider';
+
+export function AppProviders({ children }: PropsWithChildren) {
+  return <AuthProvider>{children}</AuthProvider>;
+}
