b7b803feac50b8687782c53e471d7208d10b0983
pokemongachashop <gachashop12@gmail.com>
2026-08-15 11:26:47 +0900

    AppProviders.tsx 업데이트

--- src/providers/AppProviders.tsx
+++ src/providers/AppProviders.tsx
@@ -1,12 +1,15 @@
 import type { PropsWithChildren } from 'react';
 
 import { AuthProvider } from './AuthProvider';
+import { SessionLifecycleProvider } from './SessionLifecycleProvider';
 import { UserProvider } from './UserProvider';
 
 export function AppProviders({ children }: PropsWithChildren) {
   return (
     <AuthProvider>
-      <UserProvider>{children}</UserProvider>
+      <UserProvider>
+        <SessionLifecycleProvider>{children}</SessionLifecycleProvider>
+      </UserProvider>
     </AuthProvider>
   );
 }
