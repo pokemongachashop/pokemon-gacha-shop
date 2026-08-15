8058f48158aa1732a728feb59a108c2b94f1ef82
pokemongachashop <gachashop12@gmail.com>
2026-08-15 11:23:11 +0900

    session.ts 만들기

---
+++ src/constants/session.ts
@@ -0,0 +1,15 @@
+export const AUTOMATIC_LOGOUT_MESSAGES = {
+  ACCOUNT_DISABLED:
+    '현재 사용할 수 없는 계정입니다. 관리자에게 문의해주세요.',
+
+  USER_DOCUMENT_MISSING:
+    '계정 정보를 찾을 수 없습니다. 관리자에게 문의해주세요.',
+
+  USER_DATA_INVALID:
+    '계정 정보에 문제가 있습니다. 관리자에게 문의해주세요.',
+
+  USER_ACCESS_DENIED:
+    '계정 정보에 접근할 수 없습니다. 관리자에게 문의해주세요.',
+
+  SESSION_INVALID: '로그인 상태가 올바르지 않습니다. 다시 로그인해주세요.',
+} as const;
