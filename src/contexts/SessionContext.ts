e93d18cc6a0e15948c5b1a8235022c1a1f4b1b87
pokemongachashop <gachashop12@gmail.com>
2026-08-15 11:23:57 +0900

    SessionContext.ts 만들기

---
+++ src/contexts/SessionContext.ts
@@ -0,0 +1,7 @@
+import { createContext } from 'react';
+
+import type { SessionContextValue } from '@/types';
+
+export const SessionContext = createContext<SessionContextValue | null>(
+  null,
+);
