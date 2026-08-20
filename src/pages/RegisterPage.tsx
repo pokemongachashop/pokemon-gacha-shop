f3c3244595f7a9bd2607cdf42a861db8707ef879
pokemongachashop <gachashop12@gmail.com>
2026-08-20 16:08:23 +0900

    RegisterPage.tsx 만들기

---
+++ src/pages/RegisterPage.tsx
@@ -0,0 +1,10 @@
+import { RegisterForm } from '@/components/auth/RegisterForm';
+
+export function RegisterPage() {
+  return (
+    <main>
+      <h1>회원가입</h1>
+      <RegisterForm />
+    </main>
+  );
+}
