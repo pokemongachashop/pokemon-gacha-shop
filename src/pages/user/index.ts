b1fd588fb6a7823091fb918e59ae5eccb7607521
pokemongachashop <gachashop12@gmail.com>
2026-08-21 10:56:28 +0900

    index.ts 만들기

---
+++ src/pages/user/index.ts
@@ -0,0 +1,4 @@
+export { HomePlaceholderPage } from './HomePlaceholderPage';
+export { InventoryPlaceholderPage } from './InventoryPlaceholderPage';
+export { NoticesPlaceholderPage } from './NoticesPlaceholderPage';
+export { ProfilePlaceholderPage } from './ProfilePlaceholderPage';
