50eb1c56a8969e77f45ee2ced919b498a3b448e0
pokemongachashop <gachashop12@gmail.com>
2026-08-21 11:00:41 +0900

    index.ts 만들기

---
+++ src/routes/index.ts
@@ -0,0 +1,5 @@
+export { AppRouter } from './AppRouter';
+export { AdminRoute } from './AdminRoute';
+export { ProtectedRoute } from './ProtectedRoute';
+export { PublicOnlyRoute } from './PublicOnlyRoute';
+export { RootRedirect } from './RootRedirect';
