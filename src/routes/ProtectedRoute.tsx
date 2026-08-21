fa43f69ee3fd46f8dd84dd20a34e5f93f5d682f2
pokemongachashop <gachashop12@gmail.com>
2026-08-21 10:58:25 +0900

    routes/ProtectedRoute.tsx 만들기

---
+++ src/routes/ProtectedRoute.tsx
@@ -0,0 +1,46 @@
+import {
+  Navigate,
+  useLocation,
+  type PropsWithChildren,
+} from 'react-router-dom';
+
+import { AppInitializing } from '@/components/common';
+import { ROUTES } from '@/constants';
+import { useAuth, useSession, useUser } from '@/hooks';
+
+export type ProtectedRouteProps = PropsWithChildren;
+
+export function ProtectedRoute({ children }: ProtectedRouteProps) {
+  const { isAuthenticated, isAuthInitializing } = useAuth();
+  const { userProfile, isUserLoading } = useUser();
+  const { isTerminatingSession } = useSession();
+  const location = useLocation();
+
+  if (isAuthInitializing) {
+    return <AppInitializing />;
+  }
+
+  if (!isAuthenticated) {
+    return (
+      <Navigate
+        to={ROUTES.LOGIN}
+        replace
+        state={{ from: location.pathname }}
+      />
+    );
+  }
+
+  if (isTerminatingSession) {
+    return <AppInitializing />;
+  }
+
+  if (isUserLoading) {
+    return <AppInitializing />;
+  }
+
+  if (!userProfile || !userProfile.isActive) {
+    return <AppInitializing />;
+  }
+
+  return <>{children}</>;
+}
