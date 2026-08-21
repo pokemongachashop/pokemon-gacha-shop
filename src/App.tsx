a3e2736061fcfc1eb953e11131bc0595797fb67b
pokemongachashop <gachashop12@gmail.com>
2026-08-21 11:00:55 +0900

    App.tsx 업데이트

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
+import { AppRouter } from '@/routes';
+
 function App() {
-  return (
-    <main>
-      <h1>Pokemon Gacha Shop</h1>
-      <p>Project Bootstrap Complete</p>
-    </main>
-  );
+  return <AppRouter />;
 }
 
 export default App;
